7f75677f2aba76c1f5945fff746ac25957755fc3
raidoner <maromera005@gmail.com>
2026-07-29 14:06:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ ka = ka_dict[estomago]
 
 duracion = st.sidebar.slider("Duración del consumo (horas)", 1, 30, 10)
 tasa = st.sidebar.number_input("Tasa de ingesta (g/hora)", min_value=0.5, max_value=50.0, value=15.0, step=0.5)
-tiempo_sim = st.sidebar.slider("Horas totales a simular", 10, 50, 30)
+tiempo_sim = st.sidebar.slider("Horas totales a simular", 5, 50, 30)
 
 # --- CÁLCULOS FARMACOCINÉTICOS ---
 Vd = peso * r_widmark
